a7d2a89a5e8a178a910238f46da3585bfb4f8af0
DenniJensen <s0532338@htw-berlin.de>
2013-06-05 10:01:24 +0200

    Added package for 2nd task.

--- src/main/java/Datenanalyse/Uebung1/App.java
+++ src/main/java/Datenanalyse/Uebung1/App.java
@@ -28,24 +28,42 @@ public class App {
 	/** Logger from log4j. */
 	static Logger logger = Logger.getLogger(App.class);
 
+	/** The String you like to visit and crawl. */
+	String crawlUrl;
+
+	/** The number of crawler you like to have at the same time. */
+	int numberOfCrawler;
+
 	/**
-	 * Constructs a App (application).
+	 * Constructs a App (application). The value of the parallel working crawler
+	 * will be setted on 1.
 	 */
 	public App() {
-		
+		this.numberOfCrawler = 1;
 	}
-	
+
 	/**
-	 * @param args
+	 * Constructs a App (application).
+	 * 
+	 * @param crawlUrl
+	 *            the url you wish to crawl.
+	 * @param numberOfCrawler
+	 *            the number of crawler you like to have at the same time.
+	 */
+	public App(String crawlUrl, int numberOfCrawler) {
+		this.crawlUrl = crawlUrl;
+		this.numberOfCrawler = numberOfCrawler;
+	}
+
+	/**
+	 * Runs the search engine application.
+	 * 
 	 * @throws Exception
 	 */
-	public static void main(String[] args) throws Exception {
+	public void run() throws Exception {
 		logger.setLevel(Level.DEBUG);
 		System.out.println("\nSearch Engine by Dennis Haegler\n");
 		String crawlStorageFolder = "crawl";
-		int numberOfCrawlers = 1;
-		String crawlUrl = "http://mysql12.f4.htw-berlin.de/crawl/";
-		
 
 		CrawlConfig config = new CrawlConfig();
 		config.setCrawlStorageFolder(crawlStorageFolder);
@@ -59,7 +77,7 @@ public class App {
 				pageFetcher, robotstxtServer);
 
 		crawlController.addSeed(crawlUrl);
-		crawlController.start(MyCrawler.class, numberOfCrawlers);
+		crawlController.start(MyCrawler.class, numberOfCrawler);
 		System.out.println("Finished Crawl " + crawlUrl);
 
 		// LuceneWriter storedPageWriter = new LuceneWriter("index/");
@@ -70,13 +88,23 @@ public class App {
 		}
 	}
 
-	public void run() {
-		
+	/**
+	 * @param args
+	 * @throws Exception
+	 */
+	public static void main(String[] args) throws Exception {
+		String crawlUrl = "http://mysql12.f4.htw-berlin.de/crawl/";
+		App app = new App(crawlUrl, 1);
+		app.run();
 	}
 
-	private static boolean searchIndex(String value) {
+	/**
+	 * 
+	 * @param value
+	 * @return
+	 */
+	private boolean searchIndex(String value) {
 		boolean result = true;
-
 		LuceneSearch luceneSearch = null;
 		SearchResult searchResult = null;
 		try {
